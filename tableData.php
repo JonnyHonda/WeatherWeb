dd4a8439c12758796edfec0085b325a9f8c71ee9
JonnyHonda <jonnyb3010@gmail.com>
2014-10-10 08:27:27 +0100

    Add code to revieve data from pywws

--- tableData.php
+++ tableData.php
@@ -4,11 +4,7 @@ $link = mysql_connect('sajb.co.uk', 'Tempestas', 'mRHY9BmlN4CrYUEYNhWV');
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
-$interval = 1;
-//echo 'Connected successfully';
-if(isset($_GET['interval'])){
-	$interval = $_GET['interval'];
-}
+
 $db_found = mysql_select_db("Weather");
      $myquery = "SELECT * FROM observations WHERE date >= now() - INTERVAL 5 HOUR ORDER BY id DESC";
 $query = mysql_query($myquery);
