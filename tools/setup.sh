aadf557fbb420ddad89794837b350d8c2447e9dd
root <root@4798-2526-4802.bhost.net>
2014-10-21 12:44:49 +0000

    hotfix to setup file

--- tools/setup.sh
+++ tools/setup.sh
@@ -14,4 +14,4 @@ php /var/www/html/tasks/cache-weekly-data-as-Json.php
 php /var/www/html/tasks/cache-zambretti-prediction.php
 php /var/www/html/tasks/cache-line-graph.php
 php /var/www/html/tasks/cache-multi-line-graph.php
-php /var/www/html/tasks/cache-daily-average-for-week-as-Json
+php /var/www/html/tasks/cache-daily-average-for-week-as-Json.php
