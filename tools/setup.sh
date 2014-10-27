1d01cbeef085dd3f18f95315c23bc8de10e0dbcc
JonnyHonda <jonnyb3010@gmail.com>
2014-10-27 15:59:34 +0000

    Missed setup file in last commit

--- tools/setup.sh
+++ tools/setup.sh
@@ -11,13 +11,12 @@
 
 #Execute each script to update cache files
 php /var/www/html/tasks/cache-daily-average-for-week-as-Json.php
-php /var/www/html/tasks/cache-line-graph.php
 php /var/www/html/tasks/cache-multi-line-graph.php
 php /var/www/html/tasks/cache-station-mslp-airtemp.php
 php /var/www/html/tasks/cache-table-data.php
 php /var/www/html/tasks/cache-weekly-data-as-Json.php
 php /var/www/html/tasks/cache-windrose-data.php
-php /var/www/html/tasks/cache-zambretti-prediction.php
+#php /var/www/html/tasks/cache-zambretti-prediction.php
 
 
 
