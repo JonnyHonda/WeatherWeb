8e0c03f55c50a47b872cc66164a564f89b62bd16
JonnyHonda <jonnyb3010@gmail.com>
2014-10-29 09:08:29 +0000

    Implement pulling remote json for comparison graph

---
+++ chart-data-js/highcharts.compare-remote-data.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+    $.getJSON('tasks/data/combined-soiltemps.json', function (json) {
+        chart = new Highcharts.StockChart({
+            chart: {
+                renderTo: 'garden-temperatures-multi-line-graph',
+                type: 'spline',
+                marginRight: 130,
+                marginBottom: 80,
+                zoomType: 'x'
+            },
+            title: {
+                //    text: 'Garden Temperatures',
+                //    x: -20 //center  
+            },
+            subtitle: {
+                //    text: 'Various temperatures recorded in the garden.',
+                //    x: -20
+            },
+            tooltip: {
+                backgroundColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, 'white'],
+                        [1, '#EEE']
+                    ]
+                },
+                borderColor: 'gray',
+                borderWidth: 1,
+                valueSuffix: '°C'
+            },
+            rangeSelector: {
+                selected: 0
+            },
+            dateFormat: {
+            },
+            xAxis: {
+                type: 'datetime',
+                labels: {
+                    rotation: -45
+                }
+            },
+            yAxis: {
+                title: {
+                    text: "Temp in °C"
+                },
+                plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+            },
+            legend: {
+                enabled: true,
+                itemDistance: 50
+            },
+            series: [{
+                    name: 'Local Soil Temp',
+                    stack: 'this_week',
+                    data: json.local.soil_temp_10
+                },
+                {
+                    name: 'Remote Soil Temp',
+                    stack: 'this_week',
+                   data: json.remote.soil_temp_10
+                   }
+            ]
+        });
+    });
+});
+
