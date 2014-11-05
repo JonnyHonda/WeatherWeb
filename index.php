903781aa0098712b15e5b4cb78fe3a883f6dcb63
JonnyHonda <jonnyb3010@gmail.com>
2014-11-05 13:09:14 +0000

    Add version file so version number can be displayed in the title of the home page

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>Home Weather Station</title>
+        <title>Home Weather Station - <?php include('version.txt');?></title>
 
         <!-- Bootstrap Core CSS -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
