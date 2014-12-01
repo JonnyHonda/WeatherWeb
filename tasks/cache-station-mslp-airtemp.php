2c8a1b0251aa2db120f1d590cccf1cffcccc5e10
JonnyHonda <jonnyb3010@gmail.com>
2014-12-01 13:40:26 +0000

    Update the MSLP calc to use temperature

--- tasks/cache-station-mslp-airtemp.php
+++ tasks/cache-station-mslp-airtemp.php
@@ -12,7 +12,8 @@ if (!$link) {
 $db_found = mysql_select_db(DATABASE);
 $myquery = "SELECT dateutc, round((baromin * 33.8637526) ,2) as pressure, "
         . "round((tempf-32)/1.8,2) as air_temp, "
-        . "round((dewptf-32)/1.8,2) as dew_temp "
+        . "round((dewptf-32)/1.8,2) as dew_temp, "
+        . "humidity "
         . "FROM Weather.station_data "
         . "WHERE dateutc >= now() - INTERVAL 1 YEAR ORDER BY dateutc";
 $query = mysql_query($myquery);
@@ -25,20 +26,17 @@ while ($r = mysql_fetch_array($query)) {
     $mysqldate = date("U", $phpdate) * 1000;
 
     $result['air_temp'][] = array($mysqldate, (float) $r['air_temp']);
-    $result['dew_temp'][] = array($mysqldate, (float) $r['dew_temp']);
-    $result['pressure'][] = array($mysqldate, (float) $r['pressure']);
-    $mslp = (float)round(calculateMSLP(ALTITUDE, $r['pressure'] ),2);
+    $result['pressure'][] = array($mysqldate, (float) $r['pressure']);   
+    $mslp = (float)round(calculateMSLP(ALTITUDE, $r['pressure'], $r['air_temp']),2);
     $result['mslp'][] = array($mysqldate,  $mslp);
 }
 $file = json_encode($result);
 file_put_contents('data/cache-station-mslp-airtemp.json', $file);
-  function calculateMSLP($altitude_in_meters,$pressure_in_millibars){
 
-        $F1 = (pow(1013.25, 0.190284) * 0.0065)/288;
-        $F2 = $altitude_in_meters/pow(($pressure_in_millibars - 0.3), 0.190284);
-        $F3 = 1/0.190284;
-        $F =  pow((1 + ($F1 * $F2)), $F3);
-        return  ($pressure_in_millibars - 0.3) * $F;
-
-    }
-?>
+  
+ function calculateMSLP($local_height,$abs_press,$air_temp){
+   $mslp = (1-($local_height*0.0065)/($air_temp+(0.0065*$local_height)+273.15));
+   $mslp = pow($mslp,-5.275);
+   return $abs_press*$mslp;
+     
+ }
