3691c253232c8120d5eb912096302a5883771827
JonnyHonda <jonnyb3010@gmail.com>
2014-12-17 13:20:15 +0000

    fix live readings as current value just plain wasn't

--- tasks/cache-live-latest-readings-json.php
+++ tasks/cache-live-latest-readings-json.php
@@ -12,30 +12,48 @@ if (!$link) {
 $db_found = mysql_select_db(DATABASE);
 $result['updated'] = date("d/m/Y H:i:s");
 
-$myquery = 'SELECT max(barom_mb) as max,min(barom_mb) as min,barom_mb as current FROM Weather.station_data
+$myquery = 'SELECT max(barom_mb) as max,min(barom_mb) as min FROM Weather.station_data
 WHERE dateutc >= now() - INTERVAL 1 day;';
 $query = mysql_query($myquery);
 while ($r = mysql_fetch_array($query)) {
     $result['barom']['max'] = array((float) $r['max']);
     $result['barom']['min'] = array((float) $r['min']);
+}
+
+$myquery = 'SELECT barom_mb as current FROM Weather.station_data
+WHERE dateutc >= now() - INTERVAL 3 HOUR ORDER BY dateutc DESC LIMIT 1;';
+$query = mysql_query($myquery);
+while ($r = mysql_fetch_array($query)) {
     $result['barom']['current'] = array((float) $r['current']);
 }
 
-$myquery = 'SELECT max(temp_c) as max,min(temp_c) as min,temp_c as current FROM Weather.station_data
+$myquery = 'SELECT max(temp_c) as max,min(temp_c) as min FROM Weather.station_data
 WHERE dateutc >= now() - INTERVAL 1 day;';
 $query = mysql_query($myquery);
 while ($r = mysql_fetch_array($query)) {
     $result['air_temp']['max'] = array((float) $r['max']);
     $result['air_temp']['min'] = array((float) $r['min']);
+}
+
+$myquery = 'SELECT temp_c as current FROM Weather.station_data
+WHERE dateutc >= now() - INTERVAL 3 HOUR ORDER BY dateutc DESC LIMIT 1;';
+$query = mysql_query($myquery);
+while ($r = mysql_fetch_array($query)) {
     $result['air_temp']['current'] = array((float) $r['current']);
 }
 
-$myquery = 'SELECT max(windgust_ms) as gustmax, max(windspeed_ms) as windmax,windspeed_ms as current, winddir as direction FROM Weather.station_data
+$myquery = 'SELECT max(windgust_ms) as gustmax, max(windspeed_ms) as windmax FROM Weather.station_data
 WHERE dateutc >= now() - INTERVAL 1 day;';
 $query = mysql_query($myquery);
 while ($r = mysql_fetch_array($query)) {
     $result['wind']['gust_max'] = array((float) $r['gustmax']);
     $result['wind']['speed_max'] = array((float) $r['windmax']);
+}
+
+$myquery = 'SELECT windspeed_ms as current, winddir as direction FROM Weather.station_data
+WHERE dateutc >= now() - INTERVAL 3 HOUR ORDER BY dateutc DESC LIMIT 1;';
+$query = mysql_query($myquery);
+while ($r = mysql_fetch_array($query)) {
     $result['wind']['current'] = array((float) $r['current']);
     $result['wind']['direction'] = array((float) $r['direction']);
 }
