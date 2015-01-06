d09ac57adec35d5341e6a5c2868433828b22b257
JonnyHonda <jonnyb3010@gmail.com>
2015-01-06 09:17:08 +0000

    Fix bug in the data cleanup cron where data from other columns was being used to update the wrong column

--- tasks/cleanup-127s.php
+++ tasks/cleanup-127s.php
@@ -1,6 +1,7 @@
 <?php
-$taskLocation =  realpath(dirname(__FILE__));
-chdir($taskLocation );
+
+$taskLocation = realpath(dirname(__FILE__));
+chdir($taskLocation);
 include ("../sanitize.inc.php");
 include("../config.php");
 $link = mysql_connect(SERVER, USER, PASSWORD);
@@ -9,42 +10,32 @@ if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
 $db_found = mysql_select_db(DATABASE);
+print DATABASE;
 
-$myquery = "SELECT * FROM observations WHERE `grass_temp` = -127 "
-        . "OR `air_temp` = -127 "
-        . "OR `grass_temp` = -127 "
-        . "OR `soil_temp_10` = -127 "
-        . "OR `soil_temp_30` = -127 "
-        . "OR `soil_temp_100` = -127 "
-        . "OR `concrete_temp` = -127 "
+$array_keys = array('air_temp', 'grass_temp', 'soil_temp_10', 'soil_temp_30', 'soil_temp_100', 'concrete_temp');
+
+foreach ($array_keys as $elem) {
+ $myquery = "SELECT * FROM observations WHERE `$elem` = -127 "
         . "ORDER BY date DESC";
 
-$query = mysql_query($myquery);
-$array_keys = array('air_temp','grass_temp','soil_temp_10','soil_temp_30','soil_temp_100','concrete_temp');
-while($r = mysql_fetch_assoc($query)) { 
-//        print_r ($r);
-    foreach($array_keys as $elem){
-        if($r[$elem] == -127){
- //          print $elem ." = $r[$elem] and id is {$r['id']}\n"; 
-           fetch_last_value($r['id'],$elem);
-           
-        }
-      next($r);
-      
-    }
+$query = mysql_query($myquery);   
+    while ($r = mysql_fetch_assoc($query)) {
 
+        if ($r[$elem] == -127) {
+            fetch_last_value($r['id'], $elem);
+        }
+        next($r);
+    }
 }
 
-function fetch_last_value($id,$column) {
-//SELECT `soil_temp_100` FROM observations WHERE id BETWEEN 7653-10 AND 7653
-/// AND `soil_temp_100` != -127 ORDER BY id DESC LIMIT 1;
+function fetch_last_value($id, $column) {
     $sql = "SELECT `$column` FROM observations WHERE id BETWEEN $id-10 AND $id "
-            . "AND `$column` != -127 ORDER BY id DESC  LIMIT 1";
-//    print $sql."\n";
+            . "AND `$column` != -127 ORDER BY RAND()"
+            . "  LIMIT 1";
+     print "\n$sql"; 
     $query = mysql_query($sql);
     $r = mysql_fetch_array($query);
     $sql = "UPDATE observations SET $column = {$r[0]} WHERE id = $id";
-     $query = mysql_query($sql);
-//    print $sql."\n\n";
-    //return $r[0];
+ print "\n$sql";   
+ $query = mysql_query($sql);
 }
