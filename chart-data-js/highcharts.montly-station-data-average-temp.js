d39bb5d55840dd65acf72ced52d64b33b64c5079
JonnyHonda <jonnyb3010@gmail.com>
2015-01-22 15:08:22 +0000

    Add new page with month average

---
+++ chart-data-js/highcharts.montly-station-data-average-temp.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+    $.getJSON('tasks/data/cache-monthly-station-data-average-temp.json', function (json) {
+        chart = new Highcharts.StockChart({
+            chart: {
+                renderTo: 'monthly-averages',
+                type: 'spline',
+                marginRight: 130,
+                marginBottom: 80,
+                zoomType: 'x'
+            },
+            title: {
+                text: json.title + ' Temperatures'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: null
+                }
+            },
+            tooltip: {
+                crosshairs: true,
+                shared: true,
+                valueSuffix: '°C'
+            },
+            rangeSelector: {
+                buttons: [ {
+                        type: 'day',
+                        count: 7,
+                        text: '7d'
+                    },  {
+                        type: 'all',
+                        count: 1,
+                        text: 'All'
+                    }],
+                selected: 3,
+                inputEnabled: true
+            },
+            legend: {
+            },
+            credits:
+                    {
+                        text: 'Last update: ' + json.updated,
+                        position: {
+                            align: 'left',
+                            y: -5,
+                            x: 5
+                        },
+                        style: {
+                            fontSize: '8pt'
+                        }
+                    },
+            series: [{
+                    name: 'Temperature',
+                    data: json.averages,
+                    zIndex: 1,
+                    marker: {
+                        fillColor: 'white',
+                        lineWidth: 2,
+                        lineColor: Highcharts.getOptions().colors[0]
+                    }
+                }, {
+                    name: 'Range',
+                    data: json.ranges,
+                    type: 'arearange',
+                    lineWidth: 0,
+                    linkedTo: ':previous',
+                    color: Highcharts.getOptions().colors[0],
+                    fillOpacity: 0.3,
+                    zIndex: 0
+                }]
+        });
+
+    });
+});
+
---
+++ tasks/cache-monthly-average-max-min-for-month-as-Json.php
@@ -0,0 +1,64 @@
+<?php
+$taskLocation =  realpath(dirname(__FILE__));
+chdir($taskLocation );
+include("../config.php");
+//date_default_timezone_get ();
+$link = mysql_connect(SERVER, USER, PASSWORD);
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+
+$db_found = mysql_select_db(DATABASE);
+
+/*
+ * These sqls only fetch data for a this and last week and not the past 7 days
+ */
+
+
+$thisMonthsRangeData = "SELECT date, round(min(grass_temp),2) as min_temp, round(max(grass_temp),2) as max_temp  from observations
+WHERE month(date) = month(now())
+group by day(date)";
+
+$thisMonthsAverageData = "SELECT date, round(avg(grass_temp),2) as temp  from observations
+WHERE month(date) = month(now())
+group by day(date)";
+
+$jsonarray = array();
+$result = mysql_query($thisMonthsRangeData);
+
+if (!$result) {
+    die('Invalid query: ' . mysql_error());
+}
+
+$data =  array();
+while($r = mysql_fetch_array($result)) {
+    $datetime = $r['date'];
+    $phpdate = strtotime($datetime);
+    $mysqldate = date("U", $phpdate) * 1000;
+    
+    $data = array($mysqldate,(float)$r['min_temp'],(float)$r['max_temp']);
+    $jsonarray['ranges'][] = $data;
+}
+
+$jsonarray['updated'] = date("d/m/Y H:i:s");
+$jsonarray['title'] = date("F");
+$result = mysql_query($thisMonthsAverageData);
+if (!$result) {
+    die('Invalid query: ' . mysql_error());
+}
+$data =  array();
+while($r = mysql_fetch_array($result)) { 
+    $datetime = $r['date'];
+    $phpdate = strtotime($datetime);
+    $mysqldate = date("U", $phpdate) * 1000;
+    
+    $data = array($mysqldate,(float)$r['temp']);
+    $jsonarray['averages'][] = $data;
+}
+
+
+$file =  json_encode($jsonarray);    
+file_put_contents('data/cache-monthly-station-data-average-temp.json', $file);  
+mysql_close($link);
+//echo $file;
+?>
