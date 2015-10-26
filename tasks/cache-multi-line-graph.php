f08c45624beb3d69e20ed1f360eb059cc6247d11
John Burrin <jonnyb3010@gmail.com>
2015-10-26 12:50:09 +0000

    Add Weather Station Page

---
+++ tasks/cache-multi-line-graph.php
@@ -0,0 +1,34 @@
+<?php
+
+$taskLocation = realpath(dirname(__FILE__));
+chdir($taskLocation);
+include("../config.php");
+
+$link = mysql_connect(SERVER, USER, PASSWORD);
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+
+$db_found = mysql_select_db(DATABASE);
+$myquery = 'SELECT date_format(date,"%Y-%m-%d %H:%i:00") as `date`, 
+    air_temp,soil_temp_100,soil_temp_30,soil_temp_10, grass_temp, concrete_temp 
+    FROM observations WHERE date >= now() - INTERVAL 1 YEAR ORDER BY id;';
+$query = mysql_query($myquery);
+$result['updated'] = date("d/m/Y H:i:s");
+while ($r = mysql_fetch_array($query)) {
+    $datetime = $r['date'];
+
+    $phpdate = strtotime($datetime);
+
+    $mysqldate = date("U", $phpdate) * 1000;
+
+    $result['air_temp'][] = array($mysqldate, (float) $r['air_temp']);
+    $result['grass_temp'][] = array($mysqldate, (float) $r['grass_temp']);
+    $result['concrete_temp'][] = array($mysqldate, (float) $r['concrete_temp']);
+    $result['soil_temp_10'][] = array($mysqldate, (float) $r['soil_temp_10']);
+    $result['soil_temp_30'][] = array($mysqldate, (float) $r['soil_temp_30']);
+    $result['soil_temp_100'][] = array($mysqldate, (float) $r['soil_temp_100']);
+}
+$file = json_encode($result);
+file_put_contents('data/cache-multi-line-graph.json', $file);
+mysql_close($link);
---
+++ tasks/cache-rainfall-json.php
@@ -0,0 +1,27 @@
+<?php
+
+$taskLocation = realpath(dirname(__FILE__));
+chdir($taskLocation);
+include("../config.php");
+
+$link = mysql_connect(SERVER, USER, PASSWORD);
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+
+$db_found = mysql_select_db(DATABASE);
+$myquery = 'SELECT dateutc, rain_mm, dailyrain_mm FROM Weather.station_data '
+        . 'WHERE dateutc >= now() - INTERVAL 1 YEAR '
+        . 'order by dateutc';
+$query = mysql_query($myquery);
+$result['updated'] = date("d/m/Y H:i:s");
+while ($r = mysql_fetch_array($query)) {
+    $datetime = $r['dateutc'];
+    $phpdate = strtotime($datetime);
+    $mysqldate = date("U", $phpdate) * 1000;
+    $result['rain_mm'][] = array($mysqldate, (float) $r['rain_mm']);
+    $result['dailyrain_mm'][] = array($mysqldate, (float) $r['dailyrain_mm']);
+}
+$file = json_encode($result);
+file_put_contents('data/cache-rainfall-data.json', $file);
+mysql_close($link);
