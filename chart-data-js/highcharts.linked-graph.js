32ac0aab5a6e8d19fb4b5ff0c8e28f4ee9b237f8
John Burrin <jonnyb3010@gmail.com>
2015-10-28 15:07:01 +0000

    add a linked graphs page

---
+++ chart-data-js/highcharts.linked-graph.js
@@ -0,0 +1,117 @@
+
+    /**
+     * In order to synchronize tooltips and crosshairs, override the 
+     * built-in events with handlers defined on the parent element.
+     */
+    $('#container').bind('mousemove touchmove', function (e) {
+        var chart,
+            point,
+            i;
+
+        for (i = 0; i < Highcharts.charts.length; i++) {
+            chart = Highcharts.charts[i];
+            e = chart.pointer.normalize(e); // Find coordinates within the chart
+            point = chart.series[0].searchPoint(e, true); // Get the hovered point
+
+            if (point) {
+                point.onMouseOver(); // Show the hover marker
+                chart.tooltip.refresh(point); // Show the tooltip
+                chart.xAxis[0].drawCrosshair(e, point); // Show the crosshair
+            }
+        }
+    });
+    /**
+     * Override the reset function, we don't need to hide the tooltips and crosshairs.
+     */
+    Highcharts.Pointer.prototype.reset = function () {};
+
+    /**
+     * Synchronize zooming through the setExtremes event handler.
+     */
+    function syncExtremes(e) {
+        var thisChart = this.chart;
+
+        Highcharts.each(Highcharts.charts, function (chart) {
+            if (chart !== thisChart) {
+                if (chart.xAxis[0].setExtremes) { // It is null while updating
+                    chart.xAxis[0].setExtremes(e.min, e.max);
+                }
+            }
+        });
+    }
+
+    $(document).ready(function () {
+    $.getJSON('tasks/data/cache-linked-graph-data.json', function (activity) {
+        $.each(activity.datasets, function (i, dataset) {
+            // Add X values
+            dataset.data = Highcharts.map(dataset.data, function (val, i) {
+                return [activity.xData[i], val];
+            });
+
+            $('<div class="chart">')
+                .appendTo('#container')
+                .highcharts({
+                    chart: {
+                        marginLeft: 40, // Keep all charts left aligned
+                        spacingTop: 20,
+                        spacingBottom: 20
+                        // zoomType: 'x',
+                        // pinchType: null // Disable zoom on touch devices
+                    },
+                    title: {
+                        text: dataset.name,
+                        align: 'left',
+                        margin: 0,
+                        x: 30
+                    },
+                    credits: {
+                        enabled: false
+                    },
+                    legend: {
+                        enabled: false
+                    },
+                    xAxis: {
+                        crosshair: true,
+                        events: {
+                            setExtremes: syncExtremes
+                        },
+                        labels: {
+                            format: '{value}'
+                        }
+                    },
+                    yAxis: {
+                        title: {
+                            text: null
+                        }
+                    },
+                    tooltip: {
+                        positioner: function () {
+                            return {
+                                x: this.chart.chartWidth - this.label.width, // right aligned
+                                y: -1 // align to title
+                            };
+                        },
+                        borderWidth: 0,
+                        backgroundColor: 'none',
+                        pointFormat: '{point.y}',
+                        headerFormat: '',
+                        shadow: false,
+                        style: {
+                            fontSize: '18px'
+                        },
+                        valueDecimals: dataset.valueDecimals
+                    },
+                    series: [{
+                        data: dataset.data,
+                        name: dataset.name,
+                        type: dataset.type,
+                        color: Highcharts.getOptions().colors[i],
+                        fillOpacity: 0.3,
+                        tooltip: {
+                            valueSuffix: ' ' + dataset.unit
+                        }
+                    }]
+                });
+        });
+    });
+    });
---
+++ tasks/cache-linked-graph-data.php
@@ -0,0 +1,67 @@
+<?php
+
+$taskLocation = realpath(dirname(__FILE__));
+chdir($taskLocation);
+include("../config.php");
+
+$link = mysql_connect(SERVER, USER, PASSWORD);
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+
+$db_found = mysql_select_db(DATABASE);
+$myquery = 'select `dateutc`, `temp_c`, `humidity`, `rain_mm`, `barom_mb`
+from station_data
+WHERE dateutc > now() - INTERVAL 1 DAY
+order by dateutc desc;';
+$query = mysql_query($myquery);
+$result['updated'] = date("d/m/Y H:i:s");
+$xdata = array();
+$temp_data = array();
+$rain_data = array();
+$pressure_data = array();
+$humidity_data = array();
+
+
+while ($r = mysql_fetch_array($query)) {
+    $datetime = $r['dateutc'];
+
+    $phpdate = strtotime($datetime);
+
+    $xdata[] = date("U", $phpdate) * 1000;
+    $temp_data[] = (double)$r['temp_c'];
+    $rain_data[] = (double)$r['rain_mm'];
+    $pressure_data[] = (double)$r['barom_mb'];
+    $humidity_data[] = (int)$r['humidity'];
+}
+$result['xData'] = $xdata;
+$result['datasets'][0]['name'] = "Temperature";
+$result['datasets'][0]['data'] = $temp_data;
+$result['datasets'][0]['unit'] = "Deg C";
+$result['datasets'][0]['type'] = "line";
+$result['datasets'][0]['valueDecimals'] = 2;
+
+$result['datasets'][1]['name'] = "Rain";
+$result['datasets'][1]['data'] = $rain_data;
+$result['datasets'][1]['unit'] = "mm";
+$result['datasets'][1]['type'] = "line";
+$result['datasets'][1]['valueDecimals'] = 1;
+
+
+$result['datasets'][2]['name'] = "Pressure";
+$result['datasets'][2]['data'] = $pressure_data;
+$result['datasets'][2]['unit'] = "mb";
+$result['datasets'][2]['type'] = "area";
+$result['datasets'][2]['valueDecimals'] = 1;
+
+
+$result['datasets'][3]['name'] = "Humidity";
+$result['datasets'][3]['data'] = $humidity_data;
+$result['datasets'][3]['unit'] = "%";
+$result['datasets'][3]['type'] = "area";
+$result['datasets'][3]['valueDecimals'] = 0;
+
+//var_dump($result);
+$file = json_encode($result);
+file_put_contents('data/cache-linked-graph-data.json', $file);
+mysql_close($link);
