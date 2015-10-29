ce315728bdbc4506ac74aed97488e9fa258576d2
John Burrin <jonnyb3010@gmail.com>
2015-10-29 08:04:49 +0000

    Fix the linking of the graphs in the linked graph page, also added max and min values

--- chart-data-js/highcharts.linked-graph.js
+++ chart-data-js/highcharts.linked-graph.js
@@ -1,46 +1,48 @@
 
-    /**
-     * In order to synchronize tooltips and crosshairs, override the 
-     * built-in events with handlers defined on the parent element.
-     */
-    $('#container').bind('mousemove touchmove', function (e) {
-        var chart,
-            point,
-            i;
 
-        for (i = 0; i < Highcharts.charts.length; i++) {
-            chart = Highcharts.charts[i];
-            e = chart.pointer.normalize(e); // Find coordinates within the chart
-            point = chart.series[0].searchPoint(e, true); // Get the hovered point
 
-            if (point) {
-                point.onMouseOver(); // Show the hover marker
-                chart.tooltip.refresh(point); // Show the tooltip
-                chart.xAxis[0].drawCrosshair(e, point); // Show the crosshair
+/**
+ * Synchronize zooming through the setExtremes event handler.
+ */
+function syncExtremes(e) {
+    var thisChart = this.chart;
+
+    Highcharts.each(Highcharts.charts, function (chart) {
+        if (chart !== thisChart) {
+            if (chart.xAxis[0].setExtremes) { // It is null while updating
+                chart.xAxis[0].setExtremes(e.min, e.max);
             }
         }
     });
-    /**
-     * Override the reset function, we don't need to hide the tooltips and crosshairs.
-     */
-    Highcharts.Pointer.prototype.reset = function () {};
+}
 
+$(document).ready(function () {
     /**
-     * Synchronize zooming through the setExtremes event handler.
-     */
-    function syncExtremes(e) {
-        var thisChart = this.chart;
+ * In order to synchronize tooltips and crosshairs, override the 
+ * built-in events with handlers defined on the parent element.
+ */
+$('#container').bind('mousemove touchmove', function (e) {
+    var chart,
+            point,
+            i;
 
-        Highcharts.each(Highcharts.charts, function (chart) {
-            if (chart !== thisChart) {
-                if (chart.xAxis[0].setExtremes) { // It is null while updating
-                    chart.xAxis[0].setExtremes(e.min, e.max);
-                }
-            }
-        });
+    for (i = 0; i < Highcharts.charts.length; i++) {
+        chart = Highcharts.charts[i];
+        e = chart.pointer.normalize(e); // Find coordinates within the chart
+        point = chart.series[0].searchPoint(e, true); // Get the hovered point
+
+        if (point) {
+            point.onMouseOver(); // Show the hover marker
+            chart.tooltip.refresh(point); // Show the tooltip
+            chart.xAxis[0].drawCrosshair(e, point); // Show the crosshair
+        }
     }
-
-    $(document).ready(function () {
+});
+/**
+ * Override the reset function, we don't need to hide the tooltips and crosshairs.
+ */
+Highcharts.Pointer.prototype.reset = function () {
+};
     $.getJSON('tasks/data/cache-linked-graph-data.json', function (activity) {
         $.each(activity.datasets, function (i, dataset) {
             // Add X values
@@ -49,69 +51,71 @@
             });
 
             $('<div class="chart">')
-                .appendTo('#container')
-                .highcharts({
-                    chart: {
-                        marginLeft: 40, // Keep all charts left aligned
-                        spacingTop: 20,
-                        spacingBottom: 20
-                        // zoomType: 'x',
-                        // pinchType: null // Disable zoom on touch devices
-                    },
-                    title: {
-                        text: dataset.name,
-                        align: 'left',
-                        margin: 0,
-                        x: 30
-                    },
-                    credits: {
-                        enabled: false
-                    },
-                    legend: {
-                        enabled: false
-                    },
-                    xAxis: {
-                        crosshair: true,
-                        events: {
-                            setExtremes: syncExtremes
+                    .appendTo('#container')
+                    .highcharts({
+                        chart: {
+                            marginLeft: 40, // Keep all charts left aligned
+                            spacingTop: 20,
+                            spacingBottom: 20
+                                    // zoomType: 'x',
+                                    // pinchType: null // Disable zoom on touch devices
                         },
-                        labels: {
-                            format: '{value}'
-                        }
-                    },
-                    yAxis: {
                         title: {
-                            text: null
-                        }
-                    },
-                    tooltip: {
-                        positioner: function () {
-                            return {
-                                x: this.chart.chartWidth - this.label.width, // right aligned
-                                y: -1 // align to title
-                            };
+                            text: dataset.name,
+                            align: 'left',
+                            margin: 0,
+                            x: 30
                         },
-                        borderWidth: 0,
-                        backgroundColor: 'none',
-                        pointFormat: '{point.y}',
-                        headerFormat: '',
-                        shadow: false,
-                        style: {
-                            fontSize: '18px'
+                        credits: {
+                            enabled: false
                         },
-                        valueDecimals: dataset.valueDecimals
-                    },
-                    series: [{
-                        data: dataset.data,
-                        name: dataset.name,
-                        type: dataset.type,
-                        color: Highcharts.getOptions().colors[i],
-                        fillOpacity: 0.3,
+                        legend: {
+                            enabled: false
+                        },
+                        xAxis: {
+                            crosshair: true,
+                            events: {
+                                setExtremes: syncExtremes
+                            },
+                            labels: {
+                                format: '{value}'
+                            }
+                        },
+                        yAxis: {
+                                min: dataset.min,
+                                max: dataset.max,
+                            title: {
+                                text: null
+                            }
+                        },
                         tooltip: {
-                            valueSuffix: ' ' + dataset.unit
-                        }
-                    }]
-                });
+                            positioner: function () {
+                                return {
+                                    x: this.chart.chartWidth - this.label.width, // right aligned
+                                    y: -1 // align to title
+                                };
+                            },
+                            borderWidth: 0,
+                            backgroundColor: 'none',
+                            pointFormat: '{point.y}',
+                            headerFormat: '',
+                            shadow: false,
+                            style: {
+                                fontSize: '18px'
+                            },
+                            valueDecimals: dataset.valueDecimals
+                        },
+                        series: [{
+                                data: dataset.data,
+                                name: dataset.name,
+                                type: dataset.type,
+                                color: Highcharts.getOptions().colors[i],
+                                fillOpacity: 0.3,
+                                tooltip: {
+                                    valueSuffix: ' ' + dataset.unit
+                                }
+                            }]
+                    });
         });
     });
-    });
+});
--- tasks/cache-linked-graph-data.php
+++ tasks/cache-linked-graph-data.php
@@ -13,7 +13,7 @@ $db_found = mysql_select_db(DATABASE);
 $myquery = 'select `dateutc`, `temp_c`, `humidity`, `rain_mm`, `barom_mb`
 from station_data
 WHERE dateutc > now() - INTERVAL 1 DAY
-order by dateutc desc;';
+order by dateutc ASC;';
 $query = mysql_query($myquery);
 $result['updated'] = date("d/m/Y H:i:s");
 $xdata = array();
@@ -28,7 +28,8 @@ while ($r = mysql_fetch_array($query)) {
 
     $phpdate = strtotime($datetime);
 
-    $xdata[] = date("U", $phpdate) * 1000;
+   // $xdata[] = date("U", $phpdate) * 1000;
+    $xdata[] = $r['dateutc'];
     $temp_data[] = (double)$r['temp_c'];
     $rain_data[] = (double)$r['rain_mm'];
     $pressure_data[] = (double)$r['barom_mb'];
@@ -52,6 +53,8 @@ $result['datasets'][2]['name'] = "Pressure";
 $result['datasets'][2]['data'] = $pressure_data;
 $result['datasets'][2]['unit'] = "mb";
 $result['datasets'][2]['type'] = "area";
+$result['datasets'][2]['min'] = 940;
+$result['datasets'][2]['max'] = 1040;
 $result['datasets'][2]['valueDecimals'] = 1;
 
 
@@ -59,9 +62,11 @@ $result['datasets'][3]['name'] = "Humidity";
 $result['datasets'][3]['data'] = $humidity_data;
 $result['datasets'][3]['unit'] = "%";
 $result['datasets'][3]['type'] = "area";
+$result['datasets'][3]['min'] = 40;
+$result['datasets'][3]['max'] = 100;
 $result['datasets'][3]['valueDecimals'] = 0;
 
-//var_dump($result);
+var_dump($result);
 $file = json_encode($result);
 file_put_contents('data/cache-linked-graph-data.json', $file);
 mysql_close($link);
