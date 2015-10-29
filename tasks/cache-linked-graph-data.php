be42e2bc102db46387761294e1ffdae60e70dbe1
John Burrin <jonnyb3010@gmail.com>
2015-10-29 13:09:16 +0000

    Added new graph to linked charts, accumulated rainfall

--- tasks/cache-linked-graph-data.php
+++ tasks/cache-linked-graph-data.php
@@ -10,7 +10,7 @@ if (!$link) {
 }
 
 $db_found = mysql_select_db(DATABASE);
-$myquery = 'select `dateutc`, `temp_c`, `humidity`, `rain_mm`, `barom_mb`
+$myquery = 'select `dateutc`, `temp_c`, `humidity`, `rain_mm`, `dailyrain_mm`, `barom_mb`
 from station_data
 WHERE dateutc > now() - INTERVAL 1 DAY
 order by dateutc ASC;';
@@ -29,9 +29,11 @@ while ($r = mysql_fetch_array($query)) {
     $xdata[] = date("U", $phpdate) * 1000;
     $temp_data[] = (double) $r['temp_c'];
     $rain_data[] = (double) $r['rain_mm'];
+    $dailyrain_data[] = (double) $r['dailyrain_mm'];
     $pressure_data[] = (double) $r['barom_mb'];
     $humidity_data[] = (int) $r['humidity'];
 }
+
 $result['xData'] = $xdata;
 $result['datasets'][0]['name'] = "Temperature";
 $result['datasets'][0]['data'] = $temp_data;
@@ -45,23 +47,27 @@ $result['datasets'][1]['unit'] = "mm";
 $result['datasets'][1]['type'] = "line";
 $result['datasets'][1]['valueDecimals'] = 1;
 
-
-$result['datasets'][2]['name'] = "Pressure";
-$result['datasets'][2]['data'] = $pressure_data;
-$result['datasets'][2]['unit'] = "mb";
+$result['datasets'][2]['name'] = "Rain Accumulation";
+$result['datasets'][2]['data'] = $dailyrain_data;
+$result['datasets'][2]['unit'] = "mm";
 $result['datasets'][2]['type'] = "area";
-$result['datasets'][2]['min'] = 940;
-$result['datasets'][2]['max'] = 1040;
 $result['datasets'][2]['valueDecimals'] = 1;
 
-
-$result['datasets'][3]['name'] = "Humidity";
-$result['datasets'][3]['data'] = $humidity_data;
-$result['datasets'][3]['unit'] = "%";
+$result['datasets'][3]['name'] = "Pressure";
+$result['datasets'][3]['data'] = $pressure_data;
+$result['datasets'][3]['unit'] = "mb";
 $result['datasets'][3]['type'] = "area";
-$result['datasets'][3]['min'] = 40;
-$result['datasets'][3]['max'] = 100;
-$result['datasets'][3]['valueDecimals'] = 0;
+$result['datasets'][3]['min'] = 940;
+$result['datasets'][3]['max'] = 1040;
+$result['datasets'][3]['valueDecimals'] = 1;
+
+$result['datasets'][4]['name'] = "Humidity";
+$result['datasets'][4]['data'] = $humidity_data;
+$result['datasets'][4]['unit'] = "%";
+$result['datasets'][4]['type'] = "area";
+$result['datasets'][4]['min'] = 40;
+$result['datasets'][4]['max'] = 100;
+$result['datasets'][4]['valueDecimals'] = 0;
 
 $file = json_encode($result);
 file_put_contents('data/cache-linked-graph-data.json', $file);
